6028670acdcb7262b3c1722af0a23dbf7b00d6dd
isirmt <r119306@g.kobe-kosen.ac.jp>
2024-01-18 21:21:40 +0900

    add: initial commit, header

---
+++ README.md
@@ -0,0 +1,17 @@
+# SAI Lab. ホームページ
+
+[Next.js](https://nextjs.org/)ベースのSAI lab.ホームページ
+
+## 開発
+
+クローンの後，下記のいずれかを実行
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import styles from "@/styles/components/Header.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header>
+      <nav className={styles.fixed_header}>
+        <div className={styles.header_contents}>
+          <div className={styles.icon_box}>
+            <Link href="/">
+              <Image src="/sai_logo.png" alt="SAI LOGO" fill />
+            </Link>
+          </div>
+          <div className={styles.link_box}>
+            <ul>
+              <Link href="/">メンバー一覧</Link>
+              <Link href="/">プロジェクト</Link>
+              <Link href="/">コンタクト</Link>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}

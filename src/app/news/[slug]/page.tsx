17f0769c3175413aa85bdab9f1c23e90bfb009b0
isirmt <r119306@g.kobe-kosen.ac.jp>
2024-02-17 20:11:18 +0900

    update: overall css, add: block

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 // "use client";
-import React, { useState, useEffect } from "react";
 import News from "@/components/DefaultStructure";
 import styles from "@/styles/app/news/news.module.scss";
 import Link from "next/link";

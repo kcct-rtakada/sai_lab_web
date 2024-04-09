908650d51ace709ec2ef78889b840a55c8557dfb
isirmt <r119306@g.kobe-kosen.ac.jp>
2024-04-09 22:38:53 +0900

    fix: iso format, user-select css

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -21,49 +21,49 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticPaths = [
     {
       url: "https://sai.ac",
-      lastModified,
+      lastModified: "2024-03-31T14:59:59.310Z",
       changeFrequency: "yearly",
       priority: 1.0,
     },
     {
       url: baseURL + "award",
-      lastModified,
+      lastModified: "2024-03-31T14:59:59.310Z",
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
       url: baseURL + "contact",
-      lastModified,
+      lastModified: "2024-03-31T14:59:59.310Z",
       changeFrequency: "yearly",
       priority: 0.8,
     },
     {
       url: baseURL + "member",
-      lastModified,
-      changeFrequency: "weekly",
+      lastModified: "2024-03-31T14:59:59.310Z",
+      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: baseURL + "news",
-      lastModified,
+      lastModified: "2024-03-31T14:59:59.310Z",
       changeFrequency: "daily",
       priority: 0.8,
     },
     {
       url: baseURL + "project",
-      lastModified,
+      lastModified: "2024-03-31T14:59:59.310Z",
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
       url: baseURL + "publication",
-      lastModified,
+      lastModified: "2024-03-31T14:59:59.310Z",
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
       url: baseURL + "thesis",
-      lastModified,
+      lastModified: "2024-03-31T14:59:59.310Z",
       changeFrequency: "weekly",
       priority: 0.8,
     },
@@ -75,7 +75,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   filteredProjects.forEach((item) => {
     dynamicPaths.push({
       url: baseURL + "project/" + item.id,
-      lastModified: new Date(),
+      lastModified: new Date(item.date).toISOString(),
       changeFrequency: "weekly",
       priority: 0.7,
     });
@@ -84,7 +84,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   filteredNews.forEach((item) => {
     dynamicPaths.push({
       url: baseURL + "news/" + item.id,
-      lastModified: new Date(),
+      lastModified: new Date(item.date).toISOString(),
       changeFrequency: "monthly",
       priority: 0.7,
     });

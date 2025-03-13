d2df978a8285d2e19bfddd0785ec396d53426699
isirmt <r119306@g.kobe-kosen.ac.jp>
2025-03-14 04:06:42 +0900

    add: school name(SEO)

--- src/components/common/SEO.tsx
+++ src/components/common/SEO.tsx
@@ -4,7 +4,7 @@ export type Props = Record<'title' | 'description' | 'url' | 'imageUrl', string 
 
 export default function SEO(info: Props): Metadata {
   const { title, description, url, imageUrl } = info;
-  const displayingTitle = title ? `${title} - SAI` : `SAI (髙田研究室)`;
+  const displayingTitle = title ? `${title} - SAI` : `SAI (神戸高専 髙田研究室)`;
   const displayingDescription = description ? description : '神戸市立工業高等専門学校 電子工学科 髙田研究室(SAI)';
   const displayingURL = url ? `https://sai.ac${url}` : 'https://sai.ac';
   const displayingImageURL = imageUrl ? imageUrl : 'https://sai.ac/colorful_icon.png';
@@ -19,7 +19,7 @@ export default function SEO(info: Props): Metadata {
       title: title ? title : `SAI`,
       description: displayingDescription,
       url: displayingURL,
-      siteName: 'SAI (髙田研究室)',
+      siteName: 'SAI (神戸高専電子工学科 髙田研究室)',
       type: 'website',
       images: {
         url: displayingImageURL,
